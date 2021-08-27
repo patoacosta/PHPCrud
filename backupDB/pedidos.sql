438db9e655e4c5d1d6dfa40cda50fbd9ccf007e4
joss.acosta@gmail.com <acosta.90>
2021-08-27 16:56:15 +0200

    BD File

---
+++ backupDB/pedidos.sql
@@ -0,0 +1,224 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pedidos
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `CATEGORIAID` int(11) NOT NULL,
+  `NOMBRECAT` char(50) NOT NULL,
+  PRIMARY KEY (`CATEGORIAID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (100,'CARNICOS'),(200,'LACTEOS'),(300,'LIMPIEZA'),(400,'HIGINE PERSONAL'),(500,'MEDICINAS'),(600,'COSMETICOS'),(700,'REVISTAS');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `CLIENTEID` int(11) NOT NULL,
+  `CEDULA_RUC` char(10) NOT NULL,
+  `NOMBRECIA` char(30) NOT NULL,
+  `NOMBRECONTACTO` char(50) NOT NULL,
+  `DIRECCIONCLI` char(50) NOT NULL,
+  `FAX` char(12) DEFAULT NULL,
+  `EMAIL` char(50) DEFAULT NULL,
+  `CELULAR` char(12) DEFAULT NULL,
+  `FIJO` char(12) DEFAULT NULL,
+  PRIMARY KEY (`CLIENTEID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'1890786576','SUPERMERCADO ESTRELLA','JUAN ALBAN','AV.AMAZONAS',NULL,NULL,NULL,NULL),(2,'1298765477','EL ROSADO','MARIA CORDERO','AV.AEL INCA',NULL,NULL,NULL,NULL),(3,'1009876567','DISTRIBUIDORA PRENSA','PEDRO PINTO','EL PINAR',NULL,NULL,NULL,NULL),(4,'1876090006','SU TIENDA','PABLO PONCE','AV.AMAZONAS',NULL,NULL,NULL,NULL),(5,'1893456776','SUPERMERCADO DORADO','LORENA PAZ','AV.6 DICIEMBRE',NULL,NULL,NULL,NULL),(6,'1678999891','MI COMISARIATO','ROSARIO UTRERAS','AV.AMAZONAS',NULL,NULL,NULL,NULL),(7,'1244567888','SUPERMERCADO DESCUENTO','LETICIA ORTEGA','AV.LA PRENSA',NULL,NULL,NULL,NULL),(8,'1456799022','EL DESCUENTO','JUAN TORRES','AV.PATRIA',NULL,NULL,NULL,NULL),(9,'1845677777','DE LUISE','JORGE PARRA','AV.AMAZONAS',NULL,NULL,NULL,NULL),(10,'183445667','YARBANTRELLA','PABLO POLIT','AV.REPUBLICA',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_ordenes`
+--
+
+DROP TABLE IF EXISTS `detalle_ordenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_ordenes` (
+  `ORDENID` int(11) NOT NULL,
+  `DETALLEID` int(11) NOT NULL,
+  `PRODUCTOID` int(11) NOT NULL,
+  `CANTIDAD` int(11) NOT NULL,
+  PRIMARY KEY (`ORDENID`,`DETALLEID`),
+  KEY `FK_DETALLE__PROD_DETA_PRODUCTO` (`PRODUCTOID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_ordenes`
+--
+
+LOCK TABLES `detalle_ordenes` WRITE;
+/*!40000 ALTER TABLE `detalle_ordenes` DISABLE KEYS */;
+INSERT INTO `detalle_ordenes` VALUES (1,1,1,2),(1,2,4,1),(1,3,6,1),(1,4,9,1),(2,1,10,10),(2,2,13,20),(3,1,3,10),(4,1,9,12),(5,1,1,14),(5,2,4,20),(6,1,3,12),(7,1,11,10),(8,1,2,10),(8,2,5,14),(8,3,7,10),(9,1,11,10),(10,1,1,5);
+/*!40000 ALTER TABLE `detalle_ordenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `EMPLEADOID` int(11) NOT NULL,
+  `NOMBRE` char(30) DEFAULT NULL,
+  `APELLIDO` char(30) DEFAULT NULL,
+  `FECHA_NAC` char(30) DEFAULT NULL,
+  `REPORTA_A` int(11) DEFAULT NULL,
+  `EXTENSION` int(11) DEFAULT NULL,
+  PRIMARY KEY (`EMPLEADOID`),
+  KEY `FK_EMPLEADO_REPORTA` (`REPORTA_A`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'JUAN','CRUZ','18/01/67',NULL,231),(2,'MARIO','SANCHEZ','01/03/79',1,144),(3,'VERONICA','ARIAS','23/06/77',1,234),(4,'PABLO','CELY','28/01/77',2,567),(5,'DIEGO','ANDRADE','15/05/70',2,890),(6,'JUAN','ANDRADE','17/11/76',3,230),(7,'MARIA','NOBOA','21/12/79',3,261);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ordenes`
+--
+
+DROP TABLE IF EXISTS `ordenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordenes` (
+  `ORDENID` int(11) NOT NULL,
+  `EMPLEADOID` int(11) NOT NULL,
+  `CLIENTEID` int(11) NOT NULL,
+  `FECHAORDEN` date NOT NULL,
+  `DESCUENTO` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ORDENID`),
+  KEY `FK_ORDENES_CLIEN_ORD_CLIENTES` (`CLIENTEID`),
+  KEY `FK_ORDENES_EMPLE_ORD_EMPLEADO` (`EMPLEADOID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordenes`
+--
+
+LOCK TABLES `ordenes` WRITE;
+/*!40000 ALTER TABLE `ordenes` DISABLE KEYS */;
+INSERT INTO `ordenes` VALUES (1,3,4,'2017-06-07',5),(2,3,4,'2002-06-07',10),(3,4,5,'2005-06-07',6),(4,2,6,'2006-06-07',2),(5,2,7,'2009-06-07',NULL),(6,4,5,'2012-06-07',10),(7,2,5,'2014-06-07',10),(8,3,2,'2013-06-07',10),(9,3,2,'2017-06-07',3),(10,2,2,'2018-06-07',2);
+/*!40000 ALTER TABLE `ordenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `PRODUCTOID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROVEEDORID` int(11) NOT NULL,
+  `CATEGORIAID` int(11) NOT NULL,
+  `DESCRIPCION` char(50) DEFAULT NULL,
+  `PRECIOUNIT` decimal(10,0) NOT NULL,
+  `EXISTENCIA` int(11) NOT NULL,
+  PRIMARY KEY (`PRODUCTOID`),
+  KEY `FK_PRODUCTO_CATE_PROD_CATEGORI` (`CATEGORIAID`),
+  KEY `FK_PRODUCTO_PROV_PROD_PROVEEDO` (`PROVEEDORID`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,10,100,'SALCHICHAS VIENESAS',3,200),(2,10,100,'SALAMI DE AJO',4,300),(3,10,100,'BOTON PARA ASADO',5,400),(4,20,100,'SALCHICHAS DE POLLO',3,200),(5,20,100,'JAMON DE POLLO',3,100),(6,30,200,'YOGURT NATURAL',4,80),(7,30,200,'LECHE CHOCOLATE',2,90),(8,40,200,'YOGURT DE SABORES',2,200),(9,40,200,'CREMA DE LECHE',4,30),(10,50,600,'BASE DE MAQUILLAJE',15,40),(11,50,600,'RIMMEL',13,20),(13,60,600,'SOMBRA DE OJOS',10,100),(14,90,900,'Test*',99,99);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proveedores` (
+  `PROVEEDORID` int(11) NOT NULL,
+  `NOMBREPROV` char(50) NOT NULL,
+  `CONTACTO` char(50) NOT NULL,
+  `CELUPROV` char(12) DEFAULT NULL,
+  `FIJOPROV` char(12) DEFAULT NULL,
+  PRIMARY KEY (`PROVEEDORID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` VALUES (10,'DON DIEGO','MANUEL ANDRADE','099234567','2124456'),(20,'PRONACA','JUAN PEREZ','0923434467','2124456'),(30,'TONY','JORGE BRITO','099234567','2124456'),(40,'MIRAFLORES','MARIA PAZ','098124498','2458799'),(50,'ALMAY','PEDRO GONZALEZ','097654567','2507190'),(60,'REVLON','MONICA SALAS','099245678','2609876'),(70,'YANBAL','BETY ARIAS','098124458','2450887'),(120,'JURIS','MANUEL ANDRADE','099234567','2124456'),(80,'CLEANER','MANUEL ANDRADE','099234567','2124456'),(90,'BAYER','MANUEL ANDRADE','099234567','2124456'),(110,'PALMOLIVE','MANUEL ANDRADE','099234567','2124456');
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-27 16:54:11
